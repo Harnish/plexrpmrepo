77e18d87b0f9a60be874407b1e40189f715575ef
Joseph Harnish <bigjoe1008@gmail.com>
2016-02-26 11:31:25 -0500

    fixed permissions

--- install.pl
+++ install.pl
@@ -3,24 +3,26 @@
 use strict;
 use File::Copy;
 
-if -e '/usr/bin/dnf' {
+if( -e '/usr/bin/dnf') {
     my $cmd = `dnf -y install createrepo`;
 } else {
     my $cmd = `yum -y install createrepo`;
 }
 
 my $dest = shift || '';
-if $dest eq '' {
+if( $dest eq '' ){
     print "Provide an install path: $0 /foo/bar \n";
     exit;
 }
+
 print "Installing into $dest\n";
-if ! -e $dest {
+if( ! -e $dest ){
     mkdir $dest;
 }
 
 copy("checkplexpackage.pl", "$dest/checkplexpackage.pl");
-
+my $mode = 0755;
+chmod $mode, "$dest/checkplexpackage.pl";
 
 open my $CRONTAB, ">", "plexrpm.crontab";
 print $CRONTAB "0 */2 * * *  root cd $dest  && ./checkplexpackage.pl 2>&1 >/var/log/plexupdate.log";
